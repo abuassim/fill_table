63945aa5792d0b6cacec224a90472c9c51e36573
MARWAN ALAHMADI <abuassim@hotmail.com>
2025-02-08 16:44:15 +0300

    Add files via upload

---
+++ LessonPlanWC1.1.js
@@ -0,0 +1,6 @@
+function unit11() {
+    document.getElementById('LectureClassPreparationText').value = "Put up the Classroom English poster on the board. Have the students open We Can! 2, Term 1 to pages 2 and 3. Then tell the students to watch and listen as you point to the pictures and say the commands on the Classroom English poster. Play CD 1 track 2 and repeat the commands.";
+    document.getElementById('LessonVocabulary').value = "Good idea!   Watch me   Make pairs   Let’s start!   You were great!";
+    document.getElementById('ThinkingSkills').value = "Concentration  -  Comprehension  -  Memory  -  Analysis of information  -  Application";
+    document.getElementById('LectureClassCloseText').value = "Play the CD once again and now have the students point to the pictures on pages 2 and 3 and repeat along with you and the CD.";
+    }
